6c16d94100f28a9a8c6cadcd021bd4e4348c7a17
Rada749 <radames5897@gmail.com>
2024-10-03 00:11:10 +0200

    add layout

--- healthsosvisual/src/main.jsx
+++ healthsosvisual/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { router } from './router/Router'
+import { RouterProvider } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </StrictMode>,
 )
